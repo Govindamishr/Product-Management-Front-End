9fa084f5a0a8d1f192b7fe07095079350d8fc6c0
Govindamishr <99072835+Govindamishr@users.noreply.github.com>
2023-07-31 22:25:13 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import productService from "../service/Product.service";
+import "./AddProduct.css";
+
+const Home = () => {
+  const [productList, setProductList] = useState([]);
+  const [msg, setMsg] = useState("");
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = () => {
+    productService
+      .getAllProduct()
+      .then((res) => {
+        setProductList(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deleteProduct = (id) => {
+    productService
+      .deleteProduct(id)
+      .then((res) => {
+        setMsg("Delete Sucessfully");
+        init();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <div className="container mt-3 hm">
+        <div className="row hm">
+          <div className="col-md-12 hm">
+            <div className="card hm">
+              <div className="card-header fs-3 text-center hm" style={{color:"white",backgroundColor:"indigo"}}>
+                All Product List
+                {msg && <p className="fs-4 text-center text-success">{msg}</p>}
+              </div>
+
+              <div className="card-body hm">
+                <table class="table hm">
+                  <thead>
+                    <tr>
+                      <th scope="col">Sl No</th>
+                      <th scope="col">Product Name</th>
+                      <th scope="col">Description</th>
+                      <th scope="col">Price</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {productList.map((p, num) => (
+                      <tr>
+                        <td>{num + 1}</td>
+                        <td>{p.productName}</td>
+                        <td>{p.description}</td>
+                        <td>{p.price}</td>
+                        <td>{p.status}</td>
+                        <td>
+                          <Link to={'editProduct/'+p.id} className="btn btn-sm btn-primary">
+                            Edit
+                          </Link>
+                          <button
+                            onClick={() => deleteProduct(p.id)}
+                            className="btn btn-sm btn-danger ms-1"
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
